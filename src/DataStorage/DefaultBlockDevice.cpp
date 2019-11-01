51507bb2979a3d9c3aab9dd81bbfbab49b7edebd
Carbon <carbon.szlachta@gmail.com>
2019-11-01 19:08:35 +0100

    Internal flash storage

--- src/DataStorage/DefaultBlockDevice.cpp
+++ src/DataStorage/DefaultBlockDevice.cpp
@@ -1,7 +1,7 @@
-#include "SDBlockDevice.h"
+#include "FlashIAPBlockDevice.h"
 
 BlockDevice* BlockDevice::get_default_instance()
 {
-    static SDBlockDevice sd(MBED_CONF_SD_SPI_MOSI, MBED_CONF_SD_SPI_MISO, MBED_CONF_SD_SPI_CLK, MBED_CONF_SD_SPI_CS);
-    return &sd;
+    static FlashIAPBlockDevice bd;
+    return &bd;
 }
