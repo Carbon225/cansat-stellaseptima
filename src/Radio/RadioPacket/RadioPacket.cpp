b4856364afeddad3d4e0831a6c9bb035a815a0c0
Carbon <carbon.szlachta@gmail.com>
2019-12-20 21:29:12 +0100

    Fixed range scale

--- src/Radio/RadioPacket/RadioPacket.cpp
+++ src/Radio/RadioPacket/RadioPacket.cpp
@@ -1,11 +1,11 @@
 #include "RadioPacket.h"
 
 int scaleToBits(double x, double min, double max, double range) {
-    return (x - min) * range / max;
+    return (x - min) * range / (max - min);
 }
 
 double scaleFromBits(double x, double min, double max, double range) {
-    return x * max / range + min;
+    return x * (max - min) / range + min;
 }
 
 packet_t* RadioPacket::toBinary()
