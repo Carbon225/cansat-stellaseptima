b550dd577cf2903f8ec4ce9a614221f0eb0764e6
Carbon225 <carbon.szlachta@gmail.com>
2020-01-31 18:12:30 +0100

    new gps packet

--- src/Radio/RadioPacket/GPSPacket.cpp
+++ src/Radio/RadioPacket/GPSPacket.cpp
@@ -1,13 +1,13 @@
 #include "GPSPacket.h"
 
-#define LAT_MIN 50.069082
-#define LAT_MAX 50.072783
-#define LNG_MIN 19.943569
-#define LNG_MAX 19.957398
+#define LAT_MIN 50.02111
+#define LAT_MAX 50.14055
+#define LNG_MIN 19.50527
+#define LNG_MAX 19.88
 
 GPSPacket::GPSPacket(int id, double lat, double lng)
 {
     _data = id;
-    _data |= scaleToBits(lat, LAT_MIN, LAT_MAX, 1024) << 4;
-    _data |= scaleToBits(lng, LNG_MIN, LNG_MAX, 1024) << 14;
+    _data |= scaleToBits(lat, LAT_MIN, LAT_MAX, 16383) << 4;
+    _data |= scaleToBits(lng, LNG_MIN, LNG_MAX, 16383) << 18;
 }
