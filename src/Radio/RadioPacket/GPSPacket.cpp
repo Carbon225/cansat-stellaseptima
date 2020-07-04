fb5a306f088aef7cf2f7c14fc76f5cfaee69fe19
Carbon <carbon.szlachta@gmail.com>
2020-07-04 13:54:36 +0200

    GPS areas

--- src/Radio/RadioPacket/GPSPacket.cpp
+++ src/Radio/RadioPacket/GPSPacket.cpp
@@ -1,9 +1,28 @@
 #include "GPSPacket.h"
 
+#define GPS_AREA_BALICE
+
+#ifdef GPS_AREA_RZASKA
 #define LAT_MIN 50.09425
 #define LAT_MAX 50.1087777
 #define LNG_MIN 19.8336194
 #define LNG_MAX 19.8679583
+#endif
+
+#ifdef GPS_AREA_LESZNO
+#define LAT_MIN 51.8243265
+#define LAT_MAX 51.846175
+#define LNG_MIN 16.5029
+#define LNG_MAX 16.544013
+#endif
+
+#ifdef GPS_AREA_BALICE
+#define LAT_MIN 50.02111
+#define LAT_MAX 50.1405
+#define LNG_MIN 19.50527
+#define LNG_MAX 19.88
+#endif
+
 
 GPSPacket::GPSPacket(int id, double lat, double lng)
 {
